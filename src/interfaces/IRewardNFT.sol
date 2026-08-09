7354934b3905726c8b6195ee6ffaac06c7d14db8
hooftly <mhooft@tf-dev.org>
2026-08-09 15:20:34 -0600

    feat(nft): add capped Crottos collection
    - Add the immutable Crottos ERC-721 identity and maximum supply
    - Restrict one-based sequential safe minting to the Crotto Diamond
    - Exclude burn, administrative, and public mint surfaces

--- src/interfaces/IRewardNFT.sol
+++ src/interfaces/IRewardNFT.sol
@@ -5,6 +5,11 @@ import {IERC721Metadata} from "@openzeppelin/contracts/token/ERC721/extensions/I
 
 /// @notice Fixed-maximum-supply Reward NFT with Diamond-only sequential minting.
 interface IRewardNFT is IERC721Metadata {
+    error ZeroAddress();
+    error InvalidMaxSupply();
+    error UnauthorizedMinter(address caller);
+    error MaxSupplyReached(uint256 maxSupply);
+
     function mint(address receiver) external returns (uint256 tokenId);
 
     function maxSupply() external view returns (uint256);
